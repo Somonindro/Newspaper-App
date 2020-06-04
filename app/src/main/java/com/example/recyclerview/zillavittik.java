c195d8a95bf9942c857347638be8a15cde46630e
18050 <1805056@ugrad.cse.buet.ac.bd>
2020-06-04 17:10:17 +0600

    Zilla Logos added at 6/04/2020 at 05.13pm

--- app/src/main/java/com/example/recyclerview/zillavittik.java
+++ app/src/main/java/com/example/recyclerview/zillavittik.java
@@ -16,6 +16,18 @@ public class zillavittik extends AppCompatActivity {
     private List<String> list = new ArrayList<String>();
     private RecyclerView recyclerView;
     zillaVittikRecyclerAdapter adapter;
+    final String[] districtnames=getResources().getStringArray(R.array.district);
+
+    final int[] districtimage = {R.drawable.barisal, R.drawable.bhola, R.drawable.patuakhali, R.drawable.pirojpur, R.drawable.brahmanbaria, R.drawable.chandpur,
+            R.drawable.chitagong, R.drawable.comilla, R.drawable.coxs_bazar, R.drawable.feni, R.drawable.khagrachari, R.drawable.lakshmipur,
+            R.drawable.noakhali, R.drawable.rangamati, R.drawable.dhaka, R.drawable.faridpur, R.drawable.gazipur, R.drawable.gopalganj,
+            R.drawable.kishoreganj, R.drawable.madaripur, R.drawable.munshiganj, R.drawable.narayanganj, R.drawable.narsingdi, R.drawable.rajbari,
+            R.drawable.shariatpur, R.drawable.tangail, R.drawable.bagerhat, R.drawable.chuadanga, R.drawable.jessore, R.drawable.jhenaidah, R.drawable.khulna,
+            R.drawable.kushtia, R.drawable.magura, R.drawable.meherpur, R.drawable.narail, R.drawable.sathkhira, R.drawable.jamalpur, R.drawable.mymensingh,
+            R.drawable.netrokona, R.drawable.sherpur, R.drawable.bogura, R.drawable.joypurhat, R.drawable.naogaon, R.drawable.natore, R.drawable.chapainawabganj,
+            R.drawable.pabna, R.drawable.rajshahi, R.drawable.sirajganj, R.drawable.dinajpur, R.drawable.gaibandha, R.drawable.kurigram, R.drawable.lalmonirhat,
+            R.drawable.nilfamari, R.drawable.panchagarh, R.drawable.rangpur, R.drawable.thakurgaon, R.drawable.hobiganj, R.drawable.moulvibazar, R.drawable.sunamganj,
+            R.drawable.sylhet};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +35,6 @@ public class zillavittik extends AppCompatActivity {
         setContentView(R.layout.activity_zillavittik);
 
         recyclerView = findViewById(R.id.recyclerview);
-        final String[] districtnames=getResources().getStringArray(R.array.district);
 
         for(String s:districtnames)
         {
